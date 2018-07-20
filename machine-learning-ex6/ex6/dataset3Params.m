18f5c66221ace49974e7f9ff3c3e29c7bc7bb0ec
themousepotato <navaneeths1998@gmail.com>
2018-07-21 01:44:45 +0530

    update

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+error_min = inf;
 
+for C = values
+  for sigma = values
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval));
 
+    if ( err <= error_min)
+      C_final = C;
+      sigma_final = sigma;
+      error_min = err;
+    end
+  end
+end
 
-
+C = C_final;
+sigma = sigma_final;
 
 
 % =========================================================================
